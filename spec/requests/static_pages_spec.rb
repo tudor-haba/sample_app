e3ea28fb9754947495cfa316e11a3b120f006402
Tudor Haba <htudor95@gmail.com>
2019-02-12 14:23:22 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,7 +26,7 @@ describe "StaticPages" do
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
-									:text => "Ruby on rails Tutorial Sample App | Help")
+									:text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 
 
@@ -42,7 +42,7 @@ describe "StaticPages" do
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
-									:text => "Ruby on rails Tutorial Sample App | About Us")
+									:text => "Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 
