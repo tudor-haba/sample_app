cb98fab8582b95481d879b99f4d1762aa1869751
Tudor Haba <htudor95@gmail.com>
2019-02-18 16:13:12 +0200

    CH7. Exercises completed

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,47 +1,58 @@
 require 'spec_helper'
 
-describe "UserPages" do
-		let(:user) { FactoryBot.create(:user) }
-  
+describe "UserPages" do	
 	subject { page }
 
 	describe "profile page" do
 		before { visit user_path(user) }
 
- 		it '1' do
- 			user
- 			should have_selector('h1',    text: user.name)
- 		end
-  	it { should have_selector('title', text: user.name) }
 	end	
 
 	describe "signup page" do
 		before {visit signup_path}
-
 		it { should have_selector('h1',		text: 'Sign up') }
 		it { should have_selector('title',		text: 'Sign up') }
 	end
 
 	describe "signup" do
 		before {visit signup_path}
+		
 		let(:submit) {"Create my account"}
+
 		describe "with invalid information" do
 			it "should not create a user" do
 				expect {click_button submit}.not_to change(User, :count)
 			end
+	
+
+			describe "after submission" do
+      	before { click_button submit }
+       		it { should have_selector('title', text: 'Sign up') }
+      		 it { should have_content('error') }
+     		 end
+			
 		end
 
 		describe "with valid information" do
 			before do
-				fill_in "name",				with: "Example User"
-				fill_in "Email",				with: "user@xample.com"
-				fill_in "password",				with: "foobar"
+				fill_in "Name",				with: "Example User"
+				fill_in "Email",				with: "user@example.com"
+				fill_in "Password",				with: "foobar"
 				fill_in "Confirmation",				with: "foobar"
 			end
-		end
+		
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by_email('user@example.com') }
 
-		it "should create a user" do
-			expect {click_button submit}.to change(User, :count).by(1)
-		end
+				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
+
+
+			it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+  	end
 	end
 end
