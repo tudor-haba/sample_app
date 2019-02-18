e272355f1486ad085d22ede2324d899190199a37
Tudor Haba <htudor95@gmail.com>
2019-02-18 14:34:50 +0200

    CH7. Finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
  	@user = User.find(params[:id])
  end
 
-  def new
+  def new 
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.Remember
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -34,4 +34,11 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering Bcrypt's cost function
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST 
+  end
+
 end
